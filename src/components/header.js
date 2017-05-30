b57a4f3e91727cad3b3dde442c233fc36d522361
Andrey Mamchenko <atmamont@gmail.com>
2017-05-30 07:49:15 +0300

    Initial commit

---
+++ src/components/header.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const Header = () => {
+  const { textStyle, viewStyle } = styles;
+
+  return (
+    <View style={viewStyle}>
+      <Text style={textStyle}>Albums</Text>
+    </View>
+  );
+};
+
+const styles = {
+  viewStyle: {
+    backgroundColor: '#F8F8F8',
+    justifyContent: 'center',
+    alignItems: 'center',
+
+  },
+  textStyle: {
+    fontSize: 20
+  }
+};
+
+export default Header;
